4a764070741491e7736aa5ae658baeb3dfe217f4
WOCCCDLM <wocccdlm@gmail.com>
2015-11-18 23:31:47 -0500

    Se logra que se dibuje en el canvas, y en el SVG.

--- src/main/resources/static/js/libs/appmodule.js
+++ src/main/resources/static/js/libs/appmodule.js
@@ -22,26 +22,62 @@
     });
 	};
 	
-	 $scope.consultaDatos = function() {
-		     
-        $http({
+	$scope.consultaDatos = function(boton) {    
+       
+                $scope.dibujar();
+                $scope.dibujarSVG();            
+            $http({
             method  : 'GET',
             url     : 'blueprints/'+$scope.p,
             data    : obj,
+            
 	        }).success(function(data, status, headers, config) { 
-				$scope.bluep=data; 	
-		
+				//$scope.dibujar(); 
 			});
 			 response.error(function(data, status, headers, config) {
 			alert("The petition has failed. HTTP Status:"+status);
-    });
-    };
+                        });
+        
+        };
     
     $scope.dibujar = function() {
+                alert($scope.p);
 		var cnv= document.getElementById("myCanvas");
 		var ctx = cnv.getContext("2d");
-    
+                
+                if($scope.p == "larecta"){
+                    ctx.moveTo(10,10);
+                    ctx.lineTo(10,180);
+                    ctx.strokeStyle = "blue";
+                    ctx.lineWhite = 18;
+                    ctx.stroke();
+                }
+                
+                if($scope.p == "rayon"){
+                    ctx.moveTo(40,60);
+                    ctx.lineTo(200,200);
+                    ctx.strokeStyle = "red";
+                    ctx.lineWhite = 7;
+                    ctx.stroke();
+                }
+                
+                if($scope.p == "poligono1"){
+                    ctx.fillStyle="#FF0000";
+                    ctx.fillRect    (20,20,150,100);
+                }
+                
     };
+    
+    $scope.dibujarSVG = function() {
+                alert($scope.p);
+                var cnv= document.getElementById("myCanvas");
+		var ctx = cnv.getContext("2d");
+		circulo = document.createElementNS("circulo","circle");
+                
+                
+    };
+    
+    
 
 
     }
